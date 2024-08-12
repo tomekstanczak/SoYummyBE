35104399466d6052cdbb4023f7b0339cc1345924
AWolewicz <agnieszkawolewicz5@gmail.com>
2024-08-12 14:37:13 +0200

    Swagger connected

--- routes/favorite.js
+++ routes/favorite.js
@@ -8,8 +8,74 @@ const {
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * /favorite:
+ *   get:
+ *     summary: Get a list of favorite recipes
+ *     tags: [Favorite]
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               data: [{}]
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *          application/json:
+ *            example:
+ *             error:
+ *              message: "Bad Request"
+ */
+
 router.get('/favorite', auth, getFavoriteRecipes);
+
+/**
+ * @swagger
+ * /favorite/add:
+ *   post:
+ *     summary: Add recipe to favorite list
+ *     tags: [Favorite]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               recipeId: 
+ *                 type: string
+ *     responses:
+ *       '201':
+ *         description: Success
+ */
+
 router.post('/favorite/add', auth, addFavoriteRecipe);
+
+/**
+ * @swagger
+ * /favorite/delete/:id:
+ *   delete:
+ *     summary: Delete recipe from favorite list
+ *     tags: [Favorite]
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             message: "You deleted recipe from favorite list"
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             example:
+ *               error:
+ *                 message: "Bad Request"
+ */
+
 router.delete('/favorite/delete/:id', auth, removeFavoriteRecipe);
 
 module.exports = router;
--- routes/subscribe.js
+++ routes/subscribe.js
@@ -5,6 +5,28 @@ const router = express.Router();
 const auth = require("../middleware/authenticate");
 const sendEmail = require("../controllers/subscribe/subscribeIndex");
 
+/**
+ * @swagger
+ * /:
+ *   post:
+ *     summary: Send subscribe email
+ *     tags: [Subscribe]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email: 
+ *                 type: string
+ *               name:
+ *                 type:string
+ *     responses:
+ *       '201':
+ *         description: Success
+ */
+
 router.post("/", auth, sendEmail);
 
 module.exports = router;
